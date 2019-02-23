e66ab6782de61950eb5a64a0f37cff8ef767703d
A.D-47 <abdi0361@stud.kea.dk>
2019-02-23 20:40:44 +0100

    Init Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Js online:_")
+
+countDown(5 ,60, "status");
+
+function countDown(min, seconds, element){
+
+   var min = min;
+   var element = document.getElementById(element);
+
+   element.innerHTML =" Please wait for "+min+":"+seconds+" seconds";
+   seconds--;
+   
+   if(min == 0 && seconds == 0){
+      clearTimeout(timer); 
+      alert("Your are clear to go");
+     min = 5;
+     seconds = 60;
+   }
+
+   if(seconds == 0){
+      seconds = 10;
+      min--; 
+   }
+   
+
+   var timer = setTimeout(() => {
+      countDown(min ,seconds, "status");
+   }, 1000);
+     
+}
